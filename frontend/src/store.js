f9bd8d518956eba75b489c0527a57bb341fd4af4
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-10-10 20:44:01 +0530

    added reducer for Cart items and also stored the cart items in local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,15 +3,23 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-
-const initialState = {};
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 const middleware = [thunk];
 
 const store = createStore(
