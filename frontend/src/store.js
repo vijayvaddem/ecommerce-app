b87033b98a2bf53ad1e1d7cfba1eca42ab6c9688
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-10-17 08:43:44 +0530

    UserReducer being added to manage user login logout

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,12 +4,14 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from './reducers/userReducers'
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  userLogin: userLoginReducer,
   cart: cartReducer,
 });
 
@@ -17,8 +19,13 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+  const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: {userInfo: userInfoFromStorage}
 };
 const middleware = [thunk];
 
