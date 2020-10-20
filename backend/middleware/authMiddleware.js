10bfce574bcb74b450b8c8666eeb9774adc35b92
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-10-20 09:54:06 +0530

    Implemented update user profile route in the backend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,25 +1,26 @@
-import jwt from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
-import User from '../models/userModel.js'
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        console.log('token found')
-        try {
-            token = req.headers.authorization.split(' ')[1]
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            req.user = await User.findById(decoded.id).select('-password')
-            next()
-        } catch (error) {
-            console.log("Error occurred", error)
-            res.status(401)
-            throw new Error('Not authorized, token failed')
-        }
-    } else {
-        res.status(401);
-        throw new Error('Not authorized. No token available')
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
     }
-})
+  } else {
+    res.status(401);
+    throw new Error("Not authorized. No token available");
+  }
+});
 
-export  {protect}
+export { protect };
