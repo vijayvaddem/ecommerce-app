476c8205775b59a9e3f4c2d3bfd6a5d285708ef2
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-11-10 23:31:28 +0530

    Added users list reset function to clean up the list when an admin signs out

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -20,6 +20,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -64,6 +65,9 @@ export const logout = () => (dispatch) => {
   });
   dispatch({
     type: USER_DETAILS_RESET,
+  });
+  dispatch({
+    type: USER_LIST_RESET,
   });
   dispatch({
     type: MY_ORDERS_RESET,
