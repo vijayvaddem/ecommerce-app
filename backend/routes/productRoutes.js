06f17ec8eb4212afd5f9adec689fffd71a184d92
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-11-18 23:16:04 +0530

    added new route to support delete of a product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
 import {
   getProducts,
   getProductById,
 } from "../controllers/productController.js";
 
 const router = express.Router();
-
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
