4bbdfb4665fdb9d8c1e5e0a6631ee4c5192902b1
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-11-20 00:05:39 +0530

    Problem was with the missing import for deleteProduct controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,10 +4,11 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 router.route("/").get(getProducts);
-//router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
